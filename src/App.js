1e666300ed7c9571b535f8bd9ae521878b6131f6
Abdullah Mokhtar <Abdullahmokhtr55@gmail.com>
2023-08-26 19:50:20 +0300

    added modifided files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy } from "react";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  redirect,
+} from "react-router-dom";
+
+import RootLayout from "./pages/Root";
+import HomePage from "./pages/Home";
+import { AuthContextProvider } from "./context/AuthContext";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+import EmployeeDetails from "./pages/EmployeeDetails";
+
+const AdminPage = lazy(() => import("./pages/Admin"));
+const LoginPage = lazy(() => import("./pages/Login"));
+const ErrorPage = lazy(() => import("./pages/Error"));
+
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: (
+      <AuthContextProvider>
+        <RootLayout />
+      </AuthContextProvider>
+    ),
+    children: [
+      { index: true, path: "", element: <HomePage /> },
+      {
+        path: "admin",
+        element: (
+          <ProtectedRoutes>
+            <Suspense>
+              <AdminPage />
+            </Suspense>
+          </ProtectedRoutes>
+        ),
+      },
+      {
+        path: "login",
+        element: (
+          <Suspense>
+            <LoginPage />
+          </Suspense>
+        ),
+        loader: () => {
+          if (localStorage.getItem("lll") !== null) {
+            return redirect("/");
+          }
+          return (
+            <Suspense>
+              <LoginPage />
+            </Suspense>
+          );
+        },
+      },
+      {
+        path: "/admin/:empId",
+        element: <EmployeeDetails />,
+        loader: () => {
+          if (localStorage.getItem("lll") === null) {
+            return redirect("/login");
+          } else {
+            return <EmployeeDetails />;
+          }
+        },
+      },
+      {
+        path: "*",
+        element: <ErrorPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContextProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthContextProvider>
   );
 }
 
