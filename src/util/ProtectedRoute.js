fa7d36d6d8d8854249e3c0eb03701338dc5b80a9
Abdullah Mokhtar <Abdullahmokhtr55@gmail.com>
2023-09-06 21:09:54 +0300

    puting fuc in useEffect

--- src/util/ProtectedRoute.js
+++ src/util/ProtectedRoute.js
@@ -4,18 +4,18 @@ import { useNavigate } from "react-router-dom";
 const ProtectedRoute = (props) => {
   const navigate = useNavigate();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const checkUserToken = () => {
-    const token = localStorage.getItem("token");
-    if (!token || token === undefined) {
-      setIsLoggedIn(false);
-      return navigate("/login");
-    }
-    setIsLoggedIn(true);
-  };
 
   useEffect(() => {
+    const checkUserToken = () => {
+      const token = localStorage.getItem("token");
+      if (!token || token === undefined) {
+        setIsLoggedIn(false);
+        return navigate("/login");
+      }
+      setIsLoggedIn(true);
+    };
     checkUserToken();
-  }, [isLoggedIn]);
+  }, [isLoggedIn, navigate]);
   return <>{isLoggedIn && props.children}</>;
 };
 
